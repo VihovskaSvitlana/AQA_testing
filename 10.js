3824ac43bb05764eef752724641dd832dafaa7f9
VihovskaSvitlana <svitlana.vihovska@gmail.com>
2024-02-09 13:35:22 +0100

    test node modules

--- 10.js
+++ 10.js
@@ -1,1 +1,23 @@
-10
+export const  a  = 132320;
+
+const text = 'djcdkjfhekjhf'
+
+console.log(text.substring(3,5))
+
+let rest = 5 % 2 // 1
+let a = 5;
+
+// інкремент і декримент можна використати тільки до змінної, не  до числа
+
+a++;
+++a; //  префіксний інкремент 19:36
+
+// логічні  оператори
+// || 
+
+//concat
+
+//trim
+String(varName);
+Number(varName);
+
